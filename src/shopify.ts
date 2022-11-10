d66203284f916e0dc899c46c7a19d9e62d95a003
dawid-focht <dawid.focht@frompolandwithdev.com>
2022-11-10 12:36:23 +0100

    debug messages

--- src/shopify.ts
+++ src/shopify.ts
@@ -2,6 +2,7 @@ import themeKit from "@shopify/themekit";
 import axios from "axios";
 import { shopifyThemeKitFlags } from "./github";
 import * as fs from "fs";
+import core from "@actions/core";
 
 /** Max 150 characters */
 type themeName = string;
@@ -59,7 +60,19 @@ export const getThemeByName = async (
 ): Promise<shopifyTheme | undefined> => {
   // No way to retrieve a theme by name, so retrieve all themes and find the matching theme
   const themes = await getAllThemes(SHOPIFY_AUTH);
-  return themes.find((theme) => theme.name === themeName);
+  core.debug(`Found ${themes.length} themes`);
+  for (const theme of themes) {
+    core.debug(`Found theme: ${theme.name} with ID: ${theme.id}`);
+  }
+
+  const themeWeWereLookingFor = themes.find((theme) => theme.name === themeName);
+    if (!themeWeWereLookingFor) {
+      core.warning(`Cannot find theme with name: ${themeName}`);
+    }
+    else {
+        core.info(`Found theme: ${themeWeWereLookingFor.name} with ID: ${themeWeWereLookingFor.id}`);
+    }
+  return themeWeWereLookingFor;
 };
 
 export const deployTheme = async (
@@ -76,7 +89,10 @@ export const deployTheme = async (
 };
 
 export const duplicateLiveTheme = async (SHOPIFY_AUTH: shopifyAuth, id: number): Promise<void> => {
+  core.info(`Duplicating live theme code to new theme`);
+  core.debug(`Creating tmp directory ./.shopify-tmp/`);
   !fs.existsSync(`./.shopify-tmp/`) && fs.mkdirSync(`./.shopify-tmp/`, { recursive: true });
+  core.info(`Downloading live theme code to tmp directory`);
   await themeKit.command(
     "download",
     {
@@ -88,6 +104,7 @@ export const duplicateLiveTheme = async (SHOPIFY_AUTH: shopifyAuth, id: number):
     },
     { logLevel: "all" }
   );
+    core.info(`Uploading live theme code from tmp dir to new theme`);
   await themeKit.command(
     "deploy",
     {
@@ -99,6 +116,7 @@ export const duplicateLiveTheme = async (SHOPIFY_AUTH: shopifyAuth, id: number):
     },
     { logLevel: "all" }
   );
+    core.debug(`Deleting tmp directory ./.shopify-tmp/`);
   fs.rmdirSync("./.shopify-tmp/", { recursive: true });
 };
 
@@ -111,19 +129,26 @@ export const createOrFindThemeWithName = async (
   /** Full details about the matching / created theme */
   shopifyTheme: shopifyTheme;
 }> => {
+  core.info(`Checking if theme "${shopifyThemeName}" already exists...`);
   // Theme may already exist - update the pre-existing if this is the case
   let shopifyTheme = await getThemeByName(shopifyThemeName, SHOPIFY_AUTH);
   const prexisting = !!shopifyTheme;
+  core.info(`Theme "${shopifyThemeName}" ${prexisting ? "already exists" : "does not exist"}`);
 
   // Theme does not exist in Shopify, create it
   if (!shopifyTheme) {
+    core.info(`Creating theme "${shopifyThemeName}"...`);
     await createTheme(shopifyThemeName, SHOPIFY_AUTH);
+
     shopifyTheme = await getThemeByName(shopifyThemeName, SHOPIFY_AUTH);
+
     if (!shopifyTheme) {
       throw new Error(
         `Shopify theme with name '${shopifyThemeName}' should have been created and the theme found in Shopify however the theme cannot be found in Shopify.`
       );
     }
+    core.info(`Theme "${shopifyThemeName}" created successfully`);
+
     await duplicateLiveTheme(SHOPIFY_AUTH, shopifyTheme.id);
   }
 
